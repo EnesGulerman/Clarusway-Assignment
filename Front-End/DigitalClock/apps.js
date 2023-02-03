b9d83c56ed3e35655956b1ad65bb2bf68beb6227
EnesGulerman <gulerman.enes@gmail.com>
2023-02-03 08:53:09 +0100

    digital clock complete

---
+++ Front-End/DigitalClock/apps.js
@@ -0,0 +1,58 @@
+// function displayTime(){
+//     var dateTime = new Date();
+//     var hours = dateTime.getHours();
+//     var minutes = dateTime.getMinutes();
+//     var seconds = dateTime.getSeconds();
+//     var seasion = document.getElementById('seasion');
+
+//     if(hours >= 12){
+//         seasion.innerHTML = 'PM';
+//     }else{
+//         seasion.innerHTML = 'AM';
+//     }
+
+//     if(hours < 10){
+//         hours = '0' + hours
+//     }
+
+
+//     document.getElementById('hrs').innerHTML = hours;
+//     document.getElementById('min').innerHTML = minutes;
+//     document.getElementById('sec').innerHTML = seconds;
+// }
+
+// setInterval(displayTime, 1000);
+
+var hours = document.getElementById("hrs");
+var minutes = document.getElementById("min");
+var seconds = document.getElementById("sec");
+var seasion = document.getElementById("seasion")
+
+var digitalClock = setInterval(
+    function time(){
+        var dateTime = new Date();
+        var hours = dateTime.getHours();
+        var minutes = dateTime.getMinutes();
+        var seconds = dateTime.getSeconds();
+
+
+        if(hours >= 12){
+            seasion.innerHTML = 'PM';
+            }else{
+            seasion.innerHTML = 'AM';
+            }
+        if(hours < 10){
+            hours = ("0" + hours);
+        }
+        if(minutes < 10){
+            minutes = "0" + minutes;
+        }
+        if(seconds < 10){
+            seconds = "0" + seconds;
+        }
+
+        hrs.textContent = hours;
+        min.textContent = minutes;
+        sec.textContent = seconds;
+    }, 1000
+);
