4d2da88b2efd0671b564a426bb0e63d7b1d74497
EnesGulerman <gulerman.enes@gmail.com>
2023-02-21 18:15:35 +0100

    removed

--- rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++
@@ -1,9 +0,0 @@
-const KeyboardClipboard = () => {
-  return (
-    <div className="container text-center">
-      <h2>Keyboard-Clipboard Event</h2>
-    </div>
-  )
-}
-
-export default KeyboardClipboard
