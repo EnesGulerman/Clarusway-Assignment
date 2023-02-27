86bff45aa0cb8dc6a027345a1aee7d2be32fa9f6
EnesGulerman <gulerman.enes@gmail.com>
2023-02-27 18:10:58 +0100

    Created react folder

---
+++ React/Appointment Project/my-app/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import AppointmentList from "../components/AppointmentList"
+import Doctors from "../components/Doctors"
+
+const Home = () => {
+    return (
+        <main className=""></main>
+    )
+}
